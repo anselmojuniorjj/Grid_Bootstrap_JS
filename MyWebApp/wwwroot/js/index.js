3d72a9cc4281a82bd16d890dea2660f155c7448e
Anselmo Junior <anselmojuniorjj@gmail.com>
2018-12-14 12:13:54 -0200

    Grid Bootstrap

---
+++ MyWebApp/wwwroot/js/index.js
@@ -0,0 +1,31 @@
+﻿/*
+var myButton = document.getElementById("button-Comprar");
+myButton.addEventListener("click", function () {
+    alert("teste");
+});
+---------------------------------------------------------------
+var $ = document.querySelector.bind(document);
+var myButton = $("#button-Comprar");
+myButton.addEventListener("click", function () {
+    alert("teste");
+});
+*/
+
+$(document).ready(function () {
+    var $myButton = $("#button-Comprar");
+    $myButton.on("click", function () {
+        alert("teste com jQuery");
+    });
+
+    var $precos = $(".produto li");
+    $precos.on("click", function () {
+        alert($(this).text());
+    });
+
+    var $login = $("#login");
+    var $popupForm = $("#popup-form");
+
+    $login.on("click", function () {
+        $popupForm.toggle(1000);
+    });
+});
